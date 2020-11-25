12c8751cbb1e3eed0c972ce1ccd0e5ae1573fd75
unaifg <unaifg@gmail.com>
2020-11-25 11:59:53 +0100

    BD conecta y tal

--- app/src/main/java/com/example/akirucrm/Login.java
+++ app/src/main/java/com/example/akirucrm/Login.java
@@ -11,6 +11,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Login extends AppCompatActivity {
 
@@ -19,6 +20,7 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         agregarUsuario();
+        conexionBD();
         //conecta();
     }
 
@@ -32,9 +34,20 @@ public class Login extends AppCompatActivity {
                     + "jdbc:jtds:sqlserver://192.168.1.143/northwind;instance=SQLEXPRESS;"
                     + "user=PEPE;password=1234;");*/
             //conexion = DriverManager.getConnection("jdbc:jtds:sqlserver://192.168.43.108;databaseName=HolaMundo;user=sa;password=76076015;");
-            conexion = DriverManager.getConnection("jdbc:jtds:sqlserver://192.168.56.101; instanceName=SQLEXPRESS; databaseName=CINE;  Integrated Security=False;", "AccesoDatos", "Aa1234");
+            conexion = DriverManager.getConnection("jdbc:jtds:sqlserver://192.168.56.101; Integrated Security=False;", "AccesoDatos", "Aa1234");
             //SERVER=WINSERVER\SQLEXPRESS;  Integrated Security=False; DATABASE = CINE; user=AccesoDatos; password='Aa1234'
             //Toast.makeText(getApplicationContext(), "holaaaaaa", Toast.LENGTH_LONG).show();
+
+            String bd = "CINE.dbo.Genero";
+            String sql = "insert into " + bd + " values (63, 'PEPEPEPEPEPE')";
+            try {
+                Statement stmt = null;
+                stmt = conexion.createStatement();
+                stmt.executeUpdate(sql);
+                Toast.makeText(getApplicationContext(), "To correcto", Toast.LENGTH_LONG).show();
+            }catch (SQLException e){
+                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+            }
         }
         catch (Exception e) {
             Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
@@ -59,7 +72,9 @@ public class Login extends AppCompatActivity {
     }*/
 
     public void agregarUsuario() {
-        try {
+        Connection conexion = null;
+
+        /*try {
             PreparedStatement pst = conexionBD().prepareStatement("insert into Genero values (?,?)");
             pst.setInt(1, 45);
             pst.setString(2,"Loquendo");
@@ -70,7 +85,7 @@ public class Login extends AppCompatActivity {
         catch (SQLException e) {
             Toast.makeText(this, "ERROR en el registro", Toast.LENGTH_SHORT).show();
             Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
-        }
+        }*/
     }
 
 }
